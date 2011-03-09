c14bf5624838b83592e4edd83914505a18569334
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2011-03-09 00:45:43 -0500

    Message can hold any serializable data type now. I dont like this implementation so much right now but its a step forward. This needs to be opened up beyond simply Java serialization.

--- src/main/scala/shrink/Shrink.scala
+++ src/main/scala/shrink/Shrink.scala
@@ -4,9 +4,25 @@ import akka.actor.Actor
 import akka.actor.ActorRef
 import akka.actor.Actor._
 import Actor._
+import java.io.Serializable
 
+/**
+ * A host in the system, usually a client.
+ */
 case class Host(address:String)
-case class Message(from:Host, content:String)
+
+/**
+ * A message sent from a client to be broadcasted.
+ */
+trait Message[T <: Serializable] { 
+  val from:Host
+  val data:T
+}
+
+/**
+ * String message, a message with a string data type.
+ */
+case class StringMessage(from:Host, data:String) extends Message[String]
 
 /**
  * Abstraction of Shrink relay layer for relaying messages
@@ -22,8 +38,8 @@ trait ShrinkAgent extends Actor {
   }
 
   def receive = {
-    case msg @ Message(from, content) => {
-      log.info("Got message: " + from.address + " => " + content)
+    case msg:Message[Serializable] => {
+      log.info("Got message: " + msg.from.address + " => " + msg.data)
       relay ! msg
     }
 
@@ -34,15 +50,15 @@ trait ShrinkAgent extends Actor {
 class ShrinkClient(host:String, port:Int) {
   val service = remote.actorFor("shrink-service", host, port)
 
-  def send(msg:Message) {
-    service ! Message(Host("ewp1"), "error loading stuff")
+  def send[T <: Serializable](msg:Message[T]) {
+    service ! msg
   }
 }
 
 class RedisShrinkRelay extends ShrinkRelay {
   def receive = {
-    case Message(from, content) => {
-      log.info("RedisShrinkRelay: relaying message from: " + from.address + " \"" + content + "\"")
+    case msg:Message[Serializable] => {
+      log.info("RedisShrinkRelay: relaying message from: " + msg.from.address + " \"" + msg.data + "\"")
     } 
   }
 }
